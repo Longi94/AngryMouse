d7c61388054210e45e7decab8ba9e029f7aa8297
Long Tran <lngtrn94@gmail.com>
2018-10-14 01:16:57 +0200

    add simple about page

---
+++ AngryMouse/AboutWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace AngryMouse
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppName.Content = Assembly.GetExecutingAssembly().GetName().Name;
+            AppVersion.Content = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            AppCopyright.Content = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+        }
+
+        private void Github_OnClick(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Longi94/AngryMouse");
+        }
+    }
+}
